2d4e450f88517dad85ee778a9efba9f9c346c10b
Sergei <surgery_333@mail.ru>
2024-09-14 17:59:00 +0300

    Added navbar and navbar styles

--- src/components/UI/navbar/Navbar.jsx
+++ src/components/UI/navbar/Navbar.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import classes from "./Navbar.module.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className={classes.navbar}>
+      <Link className={classes.navbar__mainLink} to="/main">
+        <img
+          className={classes.navbar__logo}
+          src="logo.png"
+          alt="Movie Randomizer Logo"
+        />
+        <span>
+          Movies
+          <br />
+          Randomizer
+        </span>
+      </Link>
+
+      <div className={classes.navbar__items}>
+        <Link className={classes.navbar__item} to="/random">
+          Randomise
+        </Link>
+        <Link className={classes.navbar__item} to="/about">
+          About
+        </Link>
+        <Link className={classes.navbar__item} to="/contacts">
+          Contacts
+        </Link>
+      </div>
     </div>
   );
 };
