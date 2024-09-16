0dad0a4db2d98d49dd5aa03d4a56a03573d3d2eb
Sergei <surgery_333@mail.ru>
2024-09-16 11:14:15 +0300

    Switched BrowserRouter on HashRouter in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./styles/App.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import Navbar from "./components/UI/navbar/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <AppRouter />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
